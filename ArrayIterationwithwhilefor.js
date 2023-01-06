8121bd26f31abe11dc6d1e9548ff9675427ba0ed
Azizbelhadjhak <belhadjazizhak6@gmail.com>
2023-01-06 21:06:35 +0100

    iteration with array

--- ArrayIterationwithwhilefor.js
+++ ArrayIterationwithwhilefor.js
@@ -1,1 +1,33 @@
+function arrayFor(array){
+    for(var i=0; i<array.length ; i++){
+        console.log (array[i])
+    }
+       
+}
 
+function arrayWhile(array){
+    var i=0
+     while ( i<array.length ){
+         console.log(array[i])
+         i++
+     }
+ }
+
+ function sum(array){
+    var result=0;
+     for(var i=0;i<array.length;i++){
+        result+=array[i];
+   
+    }      
+ return result
+ }
+
+
+ function sumEveryOther(array){
+    var result=0;
+     for(var i=0;i<array.length;i=i+2){
+    result+=array[i]
+    }      
+ return result
+ }
+ 
